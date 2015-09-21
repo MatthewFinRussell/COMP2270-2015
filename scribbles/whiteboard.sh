61789997836c7d5f1e9bf2f5e5d2f1f5531a4f65
Joey Lawrance <lawrancej@wit.edu>
2015-09-20 22:07:08 -0400

    JPG -> SVG for scribbles.

--- scribbles/whiteboard.sh
+++ scribbles/whiteboard.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "${1%.jpg}-new.jpg"
-rm "$1"
+input="$1"
+output="${input%.jpg}.bmp"
+convert "$input" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$output"
+potrace -b svg "$output"
+rm "$input"
+rm "$output"
